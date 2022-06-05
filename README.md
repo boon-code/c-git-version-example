20dd16e8b8ed0efb439d92ff081b215f52c21803
Manuel Huber <manuel.h87@gmail.com>
2022-06-05 23:29:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+See the Git Version - Example
+=============================
+
+This is an example project to show how to use
+https://github.com/boon-code/c-git-version.
---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "c_git_version.h"
+
+int main(void)
+{
+	printf("Version: %s\nBranch: %s\nHash: %s\n",
+		c_git_version(), c_git_branch(), c_git_hash());
+
+	printf("Submodules:\n%s\n", c_git_submodules());
+
+	return EXIT_SUCCESS;
+}
