015240ad20d2279d4df34f8cd5a6a06aded4aa6c
Manuel Huber <Manuel.h87@gmail.com>
2022-10-30 11:32:56 +0000

    Add doxygen to the example

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
+/** @file */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "c_git_version.h"
 
+/** @brief The main entry point
+ */
 int main(void)
 {
 	printf("Version: %s\nBranch: %s\nHash: %s\n",
